85a9856ce12f3c9980b3ec593c18397e4aecad6b
bettilaszlo <laszlobetti3@gmail.com>
2020-09-21 11:43:22 +0200

    Add project files.

---
+++ jarmu_park/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jarmu_park
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
